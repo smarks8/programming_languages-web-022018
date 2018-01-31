c2eb0bec2f56a367f0a8c761edf0202fada6f465
Stacey  Marks <staceyamarks@gmail.com>
2018-01-31 20:55:10 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,39 +1,16 @@
-# def reformat_languages(languages)
-#   new_hash = {}
-#   languages.each do |oo_or_functional, language_hash|
-#     language_hash.each do |language, attribute_hash|
-#       attribute_hash.each do |attribute, str_value|
-#         if new_hash[language].nil?
-#           new_hash[language] = {}
-#         end
-#         new_hash[language][:style] ||= []
-#         new_hash[language][:style] << oo_or_functional
-#         if new_hash[language][attribute].nil?
-#           new_hash[attribute] = str_value
-#         end
-#       end
-#     end
-#   end 
-#   new_hash
-# end
 def reformat_languages(languages)
-  language_attributes = {}
+  new_hash = {}
   languages.each do |oo_or_functional, language_hash|
-    #:oo, {:ruby => {...}}
     language_hash.each do |language, attribute_hash|
-      #:ruby, {:type => "interpreted"}
       attribute_hash.each do |attribute, str_value|
-        #:type, "interpreted"
-        if language_attributes[language].nil?
-          #if language :ruby doesn't exist in new hash
-          language_attributes[language] = {}  #initiate a new hash for the value of :ruby
+        if new_hash[language].nil?
+          new_hash[language] = {}
         end
-        # this would cause us problems if we had more than one attribute we're iterating over
-        language_attributes[language][:style] ||= []  # create a :style key within that hash and set it equal to an empty array
-        language_attributes[language][:style] << oo_or_functional  #added oo key from first level of languages hash to this key                                
+        language_attributes[language][:style] ||= [] 
+        language_attributes[language][:style] << oo_or_functional                              
         if language_attributes[language][attribute].nil?
-          #if :ruby language key doesn't have a :type key nested within it
-          language_attributes[language][attribute] = str_value  #set this :type key to equal the str "interpreted"
+
+          language_attributes[language][attribute] = str_value  
         end
       end
     end
